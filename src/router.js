3222488838be0d98f341c93df1a41c9e99fdd59e
yuanmeng <372909885@qq.com>
2018-05-01 21:23:30 +0800

    fix

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import App from './App'
 import Login from './pages/login'
 import Home from './pages/home'
 import Admin from './pages/admin'
-import Order from './pages/order'
+// import Order from './pages/order'
 
 export default class ERouter extends React.Component{
     render(){
@@ -20,7 +20,7 @@ export default class ERouter extends React.Component{
                                     <Home>
                                         <Switch>
                                             <Route exact path='/home' component={Admin}/>
-                                            <Route path='/home/order' component={Order}/>
+                                            {/* <Route path='/home/order' component={Order}/> */}
                                             {/* 失败页面重定向 */}
                                             <Redirect to='/home'/>
                                         </Switch>
